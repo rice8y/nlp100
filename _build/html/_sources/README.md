a2c1d844c2d44c56e71c87e76c7d1d6b191e8148
rice8y <eitoy1128@mbr.nifty.com>
2024-11-20 05:10:40 +0900

    update

---
+++ _build/html/_sources/README.md
@@ -0,0 +1,3 @@
+# NLP 100 Exercise 2020 (Rev 2)
+
+Click on [https://nlp100.vercel.app/](https://nlp100.vercel.app/).
--- docs/ch02/nsplit.sh
+++ docs/ch02/nsplit.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <number_of_lines> <input_file> <output_file_prefix>"
-    exit 1
-fi
-
 N=$1
 input_file=$2
-output_file_prefix=$3
+output_file_prefix=$(basename "$input_file" .txt)"-split-"
 
-split -l "$N" "$input_file" -d "$output_file_prefix"
+total_lines=$(wc -l < "$input_file")
+
+split_size=$((total_lines / N))
+rem=$((total_lines % N))
+
+if [ "$rem" -eq 0 ]; then
+    split -n "$N" -d --additional-suffix=.txt "$input_file" "$output_file_prefix"
+else
+    split -l "$((split_size + 1))" -d --additional-suffix=.txt "$input_file" "$output_file_prefix"
+fi
